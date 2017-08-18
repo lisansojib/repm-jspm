7c32f7e3f5070293b4b53e9c9fb9b8253788e959
lisansojib <lisancseru@gmail.com>
2017-08-17 23:35:52 -0700

    cors disabled in login

--- src/auth/authConfig.js
+++ src/auth/authConfig.js
@@ -1,51 +1,51 @@
 /*
 For mor visit this page: https://aurelia-authentication.spoonx.org/baseConfig.html
-*/ 
+*/
 var baseUrl = location.origin;
 if (!baseUrl)
-      baseUrl = location.protocol + "//" + location.host;
+    baseUrl = location.protocol + "//" + location.host;
 
 export default {
-  endpoint: 'auth',
-  configureEndpoints: ['auth'],
+    endpoint: 'auth',
+    configureEndpoints: ['auth'],
 
-  // SPA related options  
-  loginRedirect:'#/',                 // The SPA url to which the user is redirected after a successful login  
-  logoutRedirect:'#/login',                // The SPA url to which the user is redirected after a successful logout  
-  loginRoute:'/login',                // The SPA route used when an unauthenticated user tries to access an SPA page that requires authentication  
-  loginOnSignup: false,               // Whether or not an authentication token is provided in the response to a successful signup  
-  signupRedirect: '/home#login',          // If loginOnSignup == false: The SPA url to which the user is redirected after a successful signup (else loginRedirect is used)  
-  expiredReload: 0,                   // reload page when token expires. 0 = don't reload (default), 1 = do reload page  
-  storageChangedReload: 0,            // reload page when storage changed aka login/logout in other tabs/windows. 0 = don't reload (default), 1 = do reload page
+    // SPA related options  
+    loginRedirect: '#/listings', // The SPA url to which the user is redirected after a successful login  
+    logoutRedirect: '#/login', // The SPA url to which the user is redirected after a successful logout  
+    loginRoute: '/login', // The SPA route used when an unauthenticated user tries to access an SPA page that requires authentication  
+    loginOnSignup: false, // Whether or not an authentication token is provided in the response to a successful signup  
+    signupRedirect: '/home#login', // If loginOnSignup == false: The SPA url to which the user is redirected after a successful signup (else loginRedirect is used)  
+    expiredReload: 0, // reload page when token expires. 0 = don't reload (default), 1 = do reload page  
+    storageChangedReload: 0, // reload page when storage changed aka login/logout in other tabs/windows. 0 = don't reload (default), 1 = do reload page
 
-  // API related options  
-  baseUrl: 'http://localhost',                        // The base url used for all authentication related requests, including provider.url below. This appends to the httpClient/endpoint base url, it does not override it. 
-  loginUrl: '/token',                 // The API endpoint to which login requests are sent   
-  signupUrl: '/register',    // The API endpoint to which signup requests are sent
-  profileUrl: '/userinfo',   // The API endpoint used in profile requests (inc. `find/get` and `update`)
-  profileMethod: 'put',               // The method used to update the profile ('put' or 'patch')
-  unlinkUrl: '/unlink/',     // The API endpoint used with oAuth to unlink authentication
-  unlinkMethod: 'get',                // The HTTP method used for 'unlink' requests (Options: 'get' or 'post')
+    // API related options  
+    baseUrl: baseUrl, // The base url used for all authentication related requests, including provider.url below. This appends to the httpClient/endpoint base url, it does not override it. 
+    loginUrl: '/token', // The API endpoint to which login requests are sent   
+    signupUrl: '/register', // The API endpoint to which signup requests are sent
+    profileUrl: '/userinfo', // The API endpoint used in profile requests (inc. `find/get` and `update`)
+    profileMethod: 'put', // The method used to update the profile ('put' or 'patch')
+    unlinkUrl: '/unlink/', // The API endpoint used with oAuth to unlink authentication
+    unlinkMethod: 'get', // The HTTP method used for 'unlink' requests (Options: 'get' or 'post')
 
-  // Token Options  
-  authHeader: 'Authorization',        // The header property used to contain the authToken in the header of API requests that require authentication  
-  authTokenType: 'Bearer',            // The token name used in the header of API requests that require authentication
-  accessTokenProp: 'access_token',    // The property from which to get the access token after a successful login or signup  
+    // Token Options  
+    authHeader: 'Authorization', // The header property used to contain the authToken in the header of API requests that require authentication  
+    authTokenType: 'Bearer', // The token name used in the header of API requests that require authentication
+    accessTokenProp: 'access_token', // The property from which to get the access token after a successful login or signup  
 
-  expiredRedirect: 1,                 // redirect to logoutRedirect after token expiration
-  providers: {
-    facebook: {
-			name: 'facebook',
-			url: baseUrl + '/api-auth/addexternalloginbycode',
-			clientId: '1283775648377519',
-			authorizationEndpoint: 'https://www.facebook.com/v2.5/dialog/oauth',
-			redirectUri: window.location.origin + '/',
-			requiredUrlParams: ['display', 'scope'],
-			scope: ['email'],
-			scopeDelimiter: ',',
-			display: 'popup',
-			oauthType: '2.0',
-			popupOptions: { width: 580, height: 400 }
+    expiredRedirect: 1, // redirect to logoutRedirect after token expiration
+    providers: {
+        facebook: {
+            name: 'facebook',
+            url: baseUrl + '/api-auth/addexternalloginbycode',
+            clientId: '1283775648377519',
+            authorizationEndpoint: 'https://www.facebook.com/v2.5/dialog/oauth',
+            redirectUri: window.location.origin + '/',
+            requiredUrlParams: ['display', 'scope'],
+            scope: ['email'],
+            scopeDelimiter: ',',
+            display: 'popup',
+            oauthType: '2.0',
+            popupOptions: { width: 580, height: 400 }
+        }
     }
-  }
 };
--- src/auth/login.js
+++ src/auth/login.js
@@ -5,89 +5,87 @@ import { BootstrapFormRenderer } from 'resources/renderer/bootstrap-form-rendere
 
 @inject(ValidationControllerFactory, AuthService)
 export class Login {
-	username = '';
-	password = '';
-	controller = null;
-	rememberMe = false;
+    username = '';
+    password = '';
+    controller = null;
+    rememberMe = false;
 
-	constructor(controllerFactory, authService) {
-		debugger;
-		this.authService = authService;
-		this.controller = controllerFactory.createForCurrentScope();
-		this.controller.addRenderer(new BootstrapFormRenderer());
-	}
+    constructor(controllerFactory, authService) {
+        debugger;
+        this.authService = authService;
+        this.controller = controllerFactory.createForCurrentScope();
+        this.controller.addRenderer(new BootstrapFormRenderer());
+    }
 
-	activate() {
-		if (this.isAuthenticated) {
-			window.location.href = "#/";
-		}
-	}
+    activate() {
+        if (this.isAuthenticated) {
+            window.location.href = "#/";
+        }
+    }
 
-	submit() {
-		return this.controller.validate()
-			.then(result => {
-				debugger;
-				if (result.valid) {
-					toastr.info("You are now sign in to Puget builder");
-					this.authService.login(
-						{
-							username: this.email,
-							password: this.password,
-							grant_type: "password"
-						},
-						{
-							mode: 'cors',
-							headers: {
-								'Content-Type': 'application/x-www-form-urlencoded'
-							}
-						})
-						.then(response => {
-							debugger;
-							toastr.success('Login successful');
-							localStorage.setItem('session_token', response.session_token)
-						})
-						.catch(err => {
-							toastr.error("Invalid email or password.");
-						});
-				}
-			})
-	};
+    submit() {
+        return this.controller.validate()
+            .then(result => {
+                debugger;
+                if (result.valid) {
+                    toastr.info("You are now sign in to Puget builder");
+                    this.authService.login({
+                            username: this.email,
+                            password: this.password,
+                            grant_type: "password"
+                        }, {
+                            //mode: 'cors',
+                            headers: {
+                                'Content-Type': 'application/x-www-form-urlencoded'
+                            }
+                        })
+                        .then(response => {
+                            debugger;
+                            toastr.success('Login successful');
+                            localStorage.setItem('session_token', response.session_token)
+                        })
+                        .catch(err => {
+                            toastr.error("Invalid email or password.");
+                        });
+                }
+            })
+    };
 
-	canActivated() {
-		if (this.auth.authenticated()) {
-			window.location.href = "/";
-		}
-	}
+    canActivated() {
+        if (this.auth.authenticated()) {
+            window.location.href = "/";
+        }
+    }
 
-	authenticate(name) {
-		debugger;
-		return this.authService.authenticate(name)
-			.then(response => {
-				debugger;
-				console.log("auth response " + response);
-			})
-			.catch(err => {
-				debugger;
-				console.log(err);
-			});
-	}
+    authenticate(name) {
+        debugger;
+        return this.authService.authenticate(name)
+            .then(response => {
+                debugger;
+                console.log("auth response " + response);
+            })
+            .catch(err => {
+                debugger;
+                console.log(err);
+            });
+    }
 
-	// use computedFrom to avoid dirty checking
-	@computedFrom('authService.authenticated')
-	get isAuthenticated() {
-		return this.authService.authenticated;
-	}
+    // use computedFrom to avoid dirty checking
+    @computedFrom('authService.authenticated')
+    get isAuthenticated() {
+        return this.authService.authenticated;
+    }
 
-	setRemember() {
-		if (this.rememberMe)
-			localStorage.setItem("aurelia-remember", 'yes');
-		else
-			localStorage.removeItem("aurelia-remember");
-	}
+    setRemember() {
+        if (this.rememberMe)
+            localStorage.setItem("aurelia-remember", 'yes');
+        else
+            localStorage.removeItem("aurelia-remember");
+    }
 }
 
 
 ValidationRules
-	.ensure(a => a.email).required().email()
-	.ensure(a => a.password).required()
-	.on(Login);
+    .ensure(a => a.email).required().email()
+    .ensure(a => a.password).required()
+    .on(Login);
